24dc400df12de12203aa413896118cdbce2c193c
margarita <mzerpa@tecnosoftware.com.ar>
2022-07-15 17:19:27 -0300

    fin de video 1 CRUD-vista

--- src/components/CrudApp.js
+++ src/components/CrudApp.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+//el crud temática de los caballeros del sodiaco
+//los datos se manda llamar de una variable inicial
+const initialDb=[
+    {
+        id:1,
+        name:"Seiya",
+        constellation:"Pegaso",
+    },
+    {
+        id:2,
+        name:"Shiryu",
+        constellation:"Gragón",
+    },
+    {
+        id:3,
+        name:"Hyoga",
+        constellation:"Cisne",
+    },
+    {
+        id:4,
+        name:"Shun",
+        constellation:"Cisne",
+    },
+    {
+        id:5,
+        name:"lkki",
+        constellation:"Fenix",
+    },
+]
 
 const CrudApp = () => {
+  const [db, setDb] = useState(initialDb);
+  
   return (
     <div>
       <h2>CRUD APP</h2>
-     
+      <CrudForm/>
+      <CrudTable data={db}/>
     </div>
   );
 }
